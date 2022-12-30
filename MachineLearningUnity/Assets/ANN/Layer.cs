6cf19ef58ecd5f5298e60d56b42f19b303a97c9f
David Michael Harper <66442656+DavidMichaelH@users.noreply.github.com>
2022-12-30 12:17:21 -0800

    Added Pong Mini game

---
+++ MachineLearningUnity/Assets/ANN/Layer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Ann
+{
+    public class Layer
+    {
+        public string Name { get; set; }
+        public int NumNeurons { get { return Neurons.Count; } }
+        public List<Neuron> Neurons { get; private set; }
+
+        public int NumInputs { get; private set; }
+
+        public Activations.Activation activation;
+
+        public Layer(int numNeurons, int numNeuronInputs, List<List<double>> neuronWeights, List<double> neuronBiases, string name = "", Activations.Activation activation = Activations.Activation.Sigmoid)
+        {
+            // Validate input
+            if (numNeurons <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numNeurons), "Number of neurons must be a positive number.");
+            }
+            if (numNeuronInputs <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numNeuronInputs), "Number of neuron inputs must be a positive number.");
+            }
+            /*
+            if (neuronWeights == null || neuronWeights.Count != numNeurons)
+            {
+                Debug.Log(neuronWeights.Count.ToString() + " " + numNeurons.ToString());
+                throw new ArgumentException("Neuron weights must be a non-null list with the same length as the number of neurons.", nameof(neuronWeights));
+            }
+            if (neuronBiases == null || neuronBiases.Count != numNeurons)
+            {
+                throw new ArgumentException("Neuron biases must be a non-null list with the same length as the number of neurons.", nameof(neuronBiases));
+            }
+            */
+
+            // Initialize class variables
+            Name = name;
+            Neurons = new List<Neuron>();
+            for (int i = 0; i < numNeurons; i++)
+            {
+                Neurons.Add(new Neuron(numNeuronInputs, neuronBiases[i], neuronWeights[i], activation));
+            }
+        }
+
+        public Layer(int numNeurons, int numNeuronInputs, string name = "", Activations.Activation activation = Activations.Activation.Sigmoid)
+        {
+            // Validate input
+            if (numNeurons <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numNeurons), "Number of neurons must be a positive number.");
+            }
+            if (numNeuronInputs <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numNeuronInputs), "Number of neuron inputs must be a positive number.");
+            }
+
+            NumInputs = numNeuronInputs;
+
+            // Initialize neurons with randomly initialized weights and biases
+            Neurons = new List<Neuron>();
+            for (int i = 0; i < numNeurons; i++)
+            {
+                Neurons.Add(new Neuron(numNeuronInputs, activation));
+            }
+        }
+
+
+    }
+}
+
---
+++ MachineLearningUnity/Assets/ANN/Model.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ann
+{
+    public class Model : ANN 
+    {
+
+        public string modelName;
+     
+        public ANN Ann { get; private set; }
+
+        public Model(int numInputs, List<ModelLayer> modelLayers, double learningRate = 0.5, string modelName = "") 
+        { 
+
+            this.modelName = modelName;
+
+
+            List<Layer> layers = new List<Layer>();
+
+            int numNeurons = modelLayers[0].NumNeurons;
+            int numNeuronInputs = numInputs;
+            string name = modelLayers[0].Name;
+            Activations.Activation activation = modelLayers[0].Activation;
+
+            layers.Add(new Layer(numNeurons, numNeuronInputs, name, activation));
+
+            for (int layerIndex = 1; layerIndex < modelLayers.Count; layerIndex++)
+            {
+                numNeurons = modelLayers[layerIndex].NumNeurons;
+                numNeuronInputs = modelLayers[layerIndex-1].NumNeurons;
+                name = modelLayers[layerIndex].Name;
+                activation = modelLayers[layerIndex].Activation;
+
+                layers.Add(new Layer(numNeurons, numNeuronInputs, name, activation));
+
+            }
+
+            Initialize(layers);
+            alpha = learningRate;
+
+        }
+    }
+
+    
+    public class ModelLayer
+    {
+        public int NumNeurons { get; private set; }
+        public Activations.Activation Activation { get; private set; }
+        public string Name { get; private set; }
+        public ModelLayer(int numNeurons, Activations.Activation activation, string name = "") 
+        {
+            NumNeurons = numNeurons;
+            Activation = activation;
+            Name = name;
+        }
+
+    }
+ 
+
+}
